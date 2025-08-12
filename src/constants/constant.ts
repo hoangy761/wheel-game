460781d815da71763b279463e160c8f5d896c328
hoangy761 <hoangy761exe@gmail.com>
2025-08-12 17:11:59 +0700

    fix(Environment Variables): setup Environment Variables

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -5,7 +5,7 @@ export const LUCKY_WHEEL_CONTRACT_A8_TESTNET =
   process.env.NEXT_PUBLIC_LUCKY_WHEEL_CONTRACT_A8_TESTNET || "0x7b5EcFe4D59ba08b85e5230e3E2a2Aca7f476692";
 
 // API Configuration
-export const BUNDLER_ENDPOINT = process.env.NEXT_PUBLIC_BUNDLER_ENDPOINT || "https://api-dev.u3id.io/api/v3/";
+export const BUNDLER_ENDPOINT = process.env.NEXT_PUBLIC_BUNDLER_ENDPOINT;
 export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || "CGhQHscckkpr4D0q2ps0Sb2Dx3nmBOTGqjvKBeLdVW4";
 export const SECRET_KEY = process.env.SECRET_KEY || "";
 
@@ -13,3 +13,5 @@ export const SECRET_KEY = process.env.SECRET_KEY || "";
 export const TELEGRAM_REDIRECT_BOT =
   process.env.NEXT_PUBLIC_TELEGRAM_REDIRECT_BOT ||
   "https://t.me/layerg_ua_verification_dev_bot?start=login-1-oleR_ROyvotzwPQX1Km5LramJYN7EB5jK8NdkeqnfB4";
+
+console.log("BUNDLER_ENDPOINT", BUNDLER_ENDPOINT);
