d43e3838bcf4524c53bef6fd5261eb258a26ff36
hoangy761 <hoangy761exe@gmail.com>
2025-08-12 16:40:02 +0700

    fix(Environment Variables): setup Environment Variables

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,10 +1,15 @@
-// export const BUNDLER_ENDPOINT = 'http://localhost:3003/api/v3/';
-export const LUCKY_WHEEL_CONTRACT = "0xaB6Cdd1e0F6C884638C1BfF759EC7D1104ce3713";
-export const LUCKY_WHEEL_CONTRACT_A8_TESTNET = "0x7b5EcFe4D59ba08b85e5230e3E2a2Aca7f476692";
-export const BUNDLER_ENDPOINT = "https://api-dev.u3id.io/api/v3/";
-// export const BUNDLER_ENDPOINT = "http://localhost:3003/api/v3/";
-// export const TELEGRAM_REDIRECT_BOT='https://t.me/layer_g_auth_dev_bot?start=login-3-DhwmO7MMf1eUyTXwIrqcATdEWx2wtPKhqOQANzrRsCk'
+// Contract addresses
+export const LUCKY_WHEEL_CONTRACT =
+  process.env.NEXT_PUBLIC_LUCKY_WHEEL_CONTRACT || "0xaB6Cdd1e0F6C884638C1BfF759EC7D1104ce3713";
+export const LUCKY_WHEEL_CONTRACT_A8_TESTNET =
+  process.env.NEXT_PUBLIC_LUCKY_WHEEL_CONTRACT_A8_TESTNET || "0x7b5EcFe4D59ba08b85e5230e3E2a2Aca7f476692";
+
+// API Configuration
+export const BUNDLER_ENDPOINT = process.env.NEXT_PUBLIC_BUNDLER_ENDPOINT || "https://api-dev.u3id.io/api/v3/";
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || "CGhQHscckkpr4D0q2ps0Sb2Dx3nmBOTGqjvKBeLdVW4";
+export const SECRET_KEY = process.env.SECRET_KEY || "";
+
+// Telegram Bot Configuration
 export const TELEGRAM_REDIRECT_BOT =
+  process.env.NEXT_PUBLIC_TELEGRAM_REDIRECT_BOT ||
   "https://t.me/layerg_ua_verification_dev_bot?start=login-1-oleR_ROyvotzwPQX1Km5LramJYN7EB5jK8NdkeqnfB4";
-export const API_KEY = "CGhQHscckkpr4D0q2ps0Sb2Dx3nmBOTGqjvKBeLdVW4";
-export const SECRET_KEY = "";
